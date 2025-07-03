e210ac2c06a6452402464fe28005871195d9c8e8
Vasiliy Starikov <vasilstar97@gmail.com>
2025-07-03 03:27:23 +0300

    fix: base added to config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/shutdown-r/',
+  base: '/shutdown-r/',
   plugins: [react()],
 })
